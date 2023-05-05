07ab09851ad8abdbc9f4ebd8b3542c570c89a0e7
briandozie <doziebrian01@gmail.com>
2023-05-06 00:14:44 +0800

    added connection to database

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from pymongo import MongoClient
+
 app = Flask(__name__)
 
-@app.route("/")
+# connecting to MongoDB Atlas cloud database
+cluster = "mongodb+srv://developer:XtaPr43DKO729pfm@idstest.7xzpnqp.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(cluster)
+db = client.IDS_TEST
+
+# routes
+@app.route("/", methods = ["GET"])
 def home():
     return "Hello, Flask!"
+
+# run
+if __name__ == "__main__":
+    app.run(debug=True)
