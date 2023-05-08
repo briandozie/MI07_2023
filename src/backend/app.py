6d158c13b93061c06cfea4a3b161f74bab41ef96
briandozie <doziebrian01@gmail.com>
2023-05-08 11:29:36 +0800

    added environment variables

--- src/backend/app.py
+++ src/backend/app.py
@@ -8,17 +8,11 @@ from routes.test import testBp
 
 # connecting to MongoDB Atlas cloud database
 cluster = os.getenv("DATABASE_URL")
-clusterr = "mongodb+srv://developer:XtaPr43DKO729pfm@idstest.7xzpnqp.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(clusterr)
+client = MongoClient(cluster)
 db = client.IDS_TEST
-print(client.list_database_names)
 
 app = Flask(__name__) # creating flask app
 
 # registering blueprints for routes
 app.register_blueprint(homeBp)
 app.register_blueprint(testBp)
-
-# run app
-# if __name__ == "__main__":
-#     app.run(debug=True)
