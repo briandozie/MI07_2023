5b53f9bae82650851af47f18764395a2004ccb48
Tee Zhi Jian <20265931@student.curtin.edu.au>
2023-05-11 14:38:47 +0800

    final testing on the output

--- src/backend/routes/portScan.py
+++ src/backend/routes/portScan.py
@@ -12,11 +12,32 @@ def ScanIpAddress():
     # Scan for open ports 
     nm = nmap.PortScanner() 
     nm.scan(hosts=f"{ipAddress}", arguments=f"-{scanType}")
-    # Format the output
-    #my_lines = nm.csv().splitline()
-    #my_list = []
-   # for line in range(my_lines):
-        #line_list = line.split(";")
-      #  my_list.extend(line_list)
     
-    return nm.csv()
+    # extract the indexes for the columns
+    rows = nm.csv().strip().split("\n")
+    columns = ["host", "protocol", "port", "state"]
+    indexes = [i for i, category in enumerate(rows[0].split(";")) if category in columns]
+    
+    # extract the element for each column
+    data = []
+    for row in rows:
+      row_data = row.split(";")
+      selected_data = [row_data[i].strip() for i in indexes]
+      data.append(selected_data)
+    
+    # this is to beautify the output to be more readable
+    max_lengths = [len(column) for column in columns]
+    for row_data in data:
+      for i, element in enumerate(row_data):
+        max_lengths[i] = max(max_lengths[i], len(element))
+    
+    for i, column in enumerate(columns):
+      print(column.ljust(max_lengths[i]), end=" ")
+    print()
+
+    for row_data in data:
+      for i, element in enumerate(row_data):
+        print(element.ljust(max_lengths[i]), end=" ")
+      print()
+    
+    return None
