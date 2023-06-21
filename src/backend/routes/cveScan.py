f520b0afc870aaabb70cd898be6ed5c394dcaca3
Joshua Koh <700042062@student.curtin.edu.my>
2023-06-21 20:07:20 +0800

    Added script to scan argument

--- src/backend/routes/cveScan.py
+++ src/backend/routes/cveScan.py
@@ -7,9 +7,11 @@ cveScan = Blueprint("cveScan", __name__, url_prefix="/cveScan")
 def scanIpAddress():
     data = request.get_json()
     ipAddress = data["ipAddress"]
-    subnetMask = data["subnetMask"]
     scanType = data["scanType"]
+    script = data["script"]
 
     # Scan for CVE in target
     nm = nmap.PortScanner()
-    nm.scan(hosts=f"{ipAddress}", arguments=f"-{scanType}")
+    nm.scan(hosts=f"{ipAddress}", arguments=f"-{scanType} --script {script}")
+    
+    return nm.csv()
