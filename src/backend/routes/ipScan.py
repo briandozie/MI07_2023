6d75a3f3bee53dbaafb77728ffb19e518b3bf7a4
Lam Sheng Ying <700038882@student.curtin.edu.my>
2023-06-30 01:29:48 +0800

    test

--- src/backend/routes/ipScan.py
+++ src/backend/routes/ipScan.py
@@ -7,21 +7,27 @@ ipScan = Blueprint("ipScan", __name__, url_prefix="/ipScan")
 def scanIpAddress():
     # retrieve IP address and subnet mask from request body
     data = request.get_json()
-    ips = data.get("ips", [])
-    ipRange = data["ipRange"]
-    subnetMask = data["subnetMask"]
     scanType = data["scanType"]
     
-    startIP, endIP = ipRange.split("-")
-    nm = nmap.PortScanner()
-    hostList = []
-     # Scan individual IP addresses
-    for ip in ips:
-        nm.scan(hosts=ip, arguments=f"-{scanType}")
-        hostList.extend(nm.all_hosts())
+    if "ipAddresses" in data:
+        # Scan multiple individual IP addresses
+        ipAddresses = data["ipAddresses"]
 
-    # scan network for hosts
-    if ipRange:
+        # scan network for hosts
+        nm = nmap.PortScanner()
+        for ipAddress in ipAddresses:
+            nm.scan(hosts=ipAddress, arguments=f"-{scanType}")
+        
+        hostList = nm.all_hosts()
+        return hostList
+    elif "ipRange" in data:
+        # Scan a range of IP addresses
+        ipRange = data["ipRange"]
+        startIP, endIP = ipRange.split("-")
+        # scan network for hosts
+        nm = nmap.PortScanner()
         nm.scan(hosts=f"{startIP}-{endIP}", arguments=f"-{scanType}")
         hostList = nm.all_hosts()
-    return hostList
+        return hostList
+    else:
+        return "Invalid request format"
