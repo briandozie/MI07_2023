87472f4c466edb41511fae4f0df49015d5599009
Joshua Koh <700042062@student.curtin.edu.my>
2023-06-30 00:30:07 +0800

    Added function to remove \t in list elements

--- src/backend/routes/cveScan.py
+++ src/backend/routes/cveScan.py
@@ -11,6 +11,11 @@ def scanTargetCVE():
     scanType = data["scanType"]
     script = data["script"]
 
-    # Scan for CVE in target
-    return subprocess.run(["powershell", f"nmap -{scanType} --script {script} {ipAddress}"], 
+    # Scan for CVE in target and store it in a list
+    cveList = subprocess.run(["powershell", f"nmap -{scanType} --script {script} {ipAddress}"], 
                           shell=True, text=True, stdout=subprocess.PIPE).stdout.splitlines()
+    
+    # Function to remove \t in list elements (Needs refinement)
+    #result = ' '.join(subList.replace('\t', '    ') for subList in cveList)
+
+    return cveList
