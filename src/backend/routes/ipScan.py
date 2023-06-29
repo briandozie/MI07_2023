575bdb67fcb6fa91999122ca0c0a4f25dbd7a645
Lam Sheng Ying <700038882@student.curtin.edu.my>
2023-06-29 21:35:22 +0800

    back to ori

--- src/backend/routes/ipScan.py
+++ src/backend/routes/ipScan.py
@@ -5,26 +5,14 @@ ipScan = Blueprint("ipScan", __name__, url_prefix="/ipScan")
 
 @ipScan.post("/")
 def scanIpAddress():
-    # retrieve start address, end address, subnet mask, and scan type from request body
+    # retrieve IP address and subnet mask from request body
     data = request.get_json()
-    startAddress = data["startAddress"]
-    endAddress = data["endAddress"]
+    ipAddress = data["ipAddress"]
     subnetMask = data["subnetMask"]
     scanType = data["scanType"]
 
     # scan network for hosts
     nm = nmap.PortScanner()
-
-    # Convert the start and end addresses to integers for iteration
-    start = int(startAddress.split('.')[-1])
-    end = int(endAddress.split('.')[-1])
-
-    hostList = []
-
-    # Iterate over the range of hosts and scan each IP address
-    for i in range(start, end + 1):
-        currentAddress = startAddress.rsplit('.', 1)[0] + '.' + str(i)
-        nm.scan(hosts=f"{currentAddress}/{subnetMask}", arguments=f"-{scanType}")
-        hostList.append(nm.all_hosts())
-
+    nm.scan(hosts=f"{ipAddress}/{subnetMask}", arguments=f"-{scanType}")
+    hostList = nm.all_hosts()
     return hostList
