0c07d2ac7813722c190f3213285dbedf51e3d920
Brian Dozie <700042554@student.curtin.edu.my>
2023-07-04 16:44:19 +0800

    connected vue with flask for /home route

--- src/frontend/frontend/src/components/Home.vue
+++ src/frontend/frontend/src/components/Home.vue
@@ -5,12 +5,30 @@
 </template>
 
 <script>
+import axios from "axios"
 export default {
   name: "Home",
   data() {
     return {
-      msg: "Hi guys",
-    };
+      msg: "",
+    }
   },
-};
+  methods: {
+    getResponse() {
+      const path = "http://localhost:5000/home"
+      axios
+        .get(path)
+        .then((res) => {
+          console.log(res.data)
+          this.msg = res.data
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+  },
+  created() {
+    this.getResponse()
+  },
+}
 </script>
