48c28ccf336a9a27928d45bd9e6f53748a538bfc
Tee Zhi Jian <20265931@student.curtin.edu.au>
2023-07-28 15:47:25 +0800

    finalized changes to prettier extension

--- src/frontend/frontend/src/components/PortScan.vue
+++ src/frontend/frontend/src/components/PortScan.vue
@@ -1,149 +1,149 @@
 <template>
-  <!-- Primary Navigation Bar -->
-  <div>
-    <nav
-      class="navbar bg-dark border-bottom border-bottom-dark"
-      data-bs-theme="dark"
-    >
-      <div class="container-fluid">
-        <router-link class="navbar-brand" to="/home"
-          >SDN Intrusion & Penetration System</router-link
-        >
-        <a class="navbar-brand ms-auto" href="#">
-          <i class="bi bi-gear"></i>
-        </a>
-        <a class="navbar-brand mS-auto" href="#">
-          <i class="bi bi-person"></i>
-        </a>
-      </div>
-    </nav>
+	<!-- Primary Navigation Bar -->
+	<div>
+		<nav
+			class="navbar bg-dark border-bottom border-bottom-dark"
+			data-bs-theme="dark"
+		>
+			<div class="container-fluid">
+				<router-link class="navbar-brand" to="/home"
+					>SDN Intrusion & Penetration System</router-link
+				>
+				<a class="navbar-brand ms-auto" href="#">
+					<i class="bi bi-gear"></i>
+				</a>
+				<a class="navbar-brand mS-auto" href="#">
+					<i class="bi bi-person"></i>
+				</a>
+			</div>
+		</nav>
 
-    <!-- Secondary Navigation Bar -->
-    <nav class="navbar bg-secondary" data-bs-theme="dark">
-      <div class="container-fluid navbar-expand">
-        <div class="nav nav-underline">
-          <router-link to="/cve" class="nav-link">CVE Scan</router-link>
-          <router-link to="/service" class="nav-link">Service Scan</router-link>
-          <router-link to="/ip" class="nav-link">IP Scan</router-link>
-          <router-link to="/port" class="nav-link active"
-            >Port Scan</router-link
-          >
-          <router-link to="/dos" class="nav-link">DoS Attack</router-link>
-          <router-link to="/ddos" class="nav-link">DDoS Attack</router-link>
-        </div>
-      </div>
-    </nav>
+		<!-- Secondary Navigation Bar -->
+		<nav class="navbar bg-secondary" data-bs-theme="dark">
+			<div class="container-fluid navbar-expand">
+				<div class="nav nav-underline">
+					<router-link to="/cve" class="nav-link">CVE Scan</router-link>
+					<router-link to="/service" class="nav-link">Service Scan</router-link>
+					<router-link to="/ip" class="nav-link">IP Scan</router-link>
+					<router-link to="/port" class="nav-link active"
+						>Port Scan</router-link
+					>
+					<router-link to="/dos" class="nav-link">DoS Attack</router-link>
+					<router-link to="/ddos" class="nav-link">DDoS Attack</router-link>
+				</div>
+			</div>
+		</nav>
 
-    <!-- Page Title -->
-    <h1>Port Scan</h1>
+		<!-- Page Title -->
+		<h1>Port Scan</h1>
 
-    <form @submit="onSubmit">
-      <!-- Target network input text field -->
-      <div class="mb-3">
-        <label for="targetNetworkInput" class="form-label"
-          >Target Network</label
-        >
-        <input
-          type="text"
-          class="form-control"
-          id="targetNetworkInput"
-          placeholder="IP address"
-          v-model="portScanForm.ipAddress"
-        />
-      </div>
+		<form @submit="onSubmit">
+			<!-- Target network input text field -->
+			<div class="mb-3">
+				<label for="targetNetworkInput" class="form-label"
+					>Target Network</label
+				>
+				<input
+					type="text"
+					class="form-control"
+					id="targetNetworkInput"
+					placeholder="IP address"
+					v-model="portScanForm.ipAddress"
+				/>
+			</div>
 
-      <!-- Scan type dropdown menu -->
-      <label for="scanTypeInput" class="form-label">Scan Type</label>
-      <select
-        class="form-select"
-        id="scanTypeInput"
-        aria-label="Default select example"
-        v-model="portScanForm.scanType"
-      >
-        <option disabled value="">Select Scan Type</option>
-        <option value="sV">TCP</option>
-        <option value="sU">UDP</option>
-      </select>
+			<!-- Scan type dropdown menu -->
+			<label for="scanTypeInput" class="form-label">Scan Type</label>
+			<select
+				class="form-select"
+				id="scanTypeInput"
+				aria-label="Default select example"
+				v-model="portScanForm.scanType"
+			>
+				<option disabled value="">Select Scan Type</option>
+				<option value="sV">TCP</option>
+				<option value="sU">UDP</option>
+			</select>
 
-      <!-- Run button -->
-      <div class="run-button">
-        <button type="submit" class="btn btn-primary">Run</button>
-        <!-- <button type="submit" class="btn btn-primary float-end">Run</button> -->
-      </div>
-    </form>
+			<!-- Run button -->
+			<div class="run-button">
+				<button type="submit" class="btn btn-primary">Run</button>
+				<!-- <button type="submit" class="btn btn-primary float-end">Run</button> -->
+			</div>
+		</form>
 
-    <!-- Scan Result -->
-    <label for="resultOutput" class="form-label">Scan Result</label>
-    <div class="card">
-      <div class="card-body">{{ result }}</div>
-    </div>
-  </div>
+		<!-- Scan Result -->
+		<label for="resultOutput" class="form-label">Scan Result</label>
+		<div class="card">
+			<div class="card-body">{{ result }}</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import axios from "axios"
 export default {
-  name: "PortScan",
-  data() {
-    return {
-      portScanForm: {
-        ipAddress: "",
-        scanType: "",
-      },
-      result: "",
-    }
-  },
-  methods: {
-    // POST Function
-    scanPorts(payload) {
-      const path = "http://localhost:5000/portScan/"
-      axios
-        .post(path, payload)
-        .then((res) => {
-          console.log(res.data)
-          this.result = res.data
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-    },
-    initForm() {
-      this.portScanForm.ipAddress = ""
-      this.portScanForm.scanType = ""
-    },
-    onSubmit(e) {
-      e.preventDefault()
-      const payload = {
-        ipAddress: this.portScanForm.ipAddress,
-        scanType: this.portScanForm.scanType,
-      }
-      console.log(payload)
-      this.scanPorts(payload)
-      this, this.initForm()
-    },
-  },
-  created() {},
+	name: "PortScan",
+	data() {
+		return {
+			portScanForm: {
+				ipAddress: "",
+				scanType: "",
+			},
+			result: "",
+		}
+	},
+	methods: {
+		// POST Function
+		scanPorts(payload) {
+			const path = "http://localhost:5000/portScan/"
+			axios
+				.post(path, payload)
+				.then((res) => {
+					console.log(res.data)
+					this.result = res.data
+				})
+				.catch((err) => {
+					console.log(err)
+				})
+		},
+		initForm() {
+			this.portScanForm.ipAddress = ""
+			this.portScanForm.scanType = ""
+		},
+		onSubmit(e) {
+			e.preventDefault()
+			const payload = {
+				ipAddress: this.portScanForm.ipAddress,
+				scanType: this.portScanForm.scanType,
+			}
+			console.log(payload)
+			this.scanPorts(payload)
+			this, this.initForm()
+		},
+	},
+	created() {},
 }
 </script>
 
 <style>
 .navbar {
-  height: 50px;
+	height: 50px;
 }
 h1 {
-  display: block;
-  padding-top: 10px;
-  margin-left: 15px;
+	display: block;
+	padding-top: 10px;
+	margin-left: 15px;
 }
 .btn-primary {
-  min-width: 100px;
-  max-width: 100px;
+	min-width: 100px;
+	max-width: 100px;
 }
 .run-button {
-  padding-top: 50px;
-  padding-bottom: 30px;
+	padding-top: 50px;
+	padding-bottom: 30px;
 }
 .card {
-  min-height: 100px;
+	min-height: 100px;
 }
 </style>
