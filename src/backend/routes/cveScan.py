d78cc6e67cf7c4da816892d95e8771dbff574c02
Joshua Koh <700042062@student.curtin.edu.my>
2023-08-02 00:12:57 +0800

    Added line formatting in cveScan.py

--- src/backend/routes/cveScan.py
+++ src/backend/routes/cveScan.py
@@ -16,6 +16,6 @@ def scanTargetCVE():
                           shell=True, text=True, stdout=subprocess.PIPE).stdout.splitlines()
     
     # Function to remove \t in list elements (Needs refinement)
-    #result = ' '.join(subList.replace('\t', '    ') for subList in cveList)
+    result = '\n'.join(subList.replace('\t', '    ') for subList in cveList)
 
-    return cveList
+    return result
