77f5f0fcb1cc8a136f62d0691630d8143a20a122
briandozie <700042554@student.curtin.edu.my>
2023-08-11 01:01:35 +0800

    migrate from windows to linux platform

--- src/frontend/frontend/src/components/PortScan.vue
+++ src/frontend/frontend/src/components/PortScan.vue
@@ -178,7 +178,7 @@ export default {
 	methods: {
 		// POST Function
 		scanPorts(payload) {
-			const path = "http://localhost:5000/portScan/"
+			const path = "http://127.0.0.1:5000/portScan/"
 			this.startTimer() // start timer
 			this.initStatus()
 			this.eventLog += `Scan started on network "${this.portScanForm.ipAddress}"\n`
