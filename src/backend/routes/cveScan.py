1e145004b8dda8e4fbf2b227847d407c71622016
Joshua Koh <700042062@student.curtin.edu.my>
2023-08-30 23:29:10 +0800

    Removed comments

--- src/backend/routes/cveScan.py
+++ src/backend/routes/cveScan.py
@@ -11,21 +11,12 @@ def scanTargetCVE():
     script = data["script"]
 
     # Scan for CVE in target and store it in a list
-    cveList = subprocess.run([f"nmap -{scanType} --script {script} {ipAddress}"], 
+    cveList = subprocess.run([f"nmap -{scanType} -D RND,RND,ME --script {script} {ipAddress}"], 
                           shell=True, text=True, stdout=subprocess.PIPE).stdout.splitlines()
     
     # Takes only the results for easier formatting
     cveList = cveList[4:-2]
     result = '\n'.join(subList.replace('\t', '    ') for subList in cveList)
-    '''
-    results = []
-    for line in cveList[4:-2]:
-        columns = line.replace('\t', '    ').split('    ')
-        host = columns[0]
-        port = columns[1]
-        cve = columns[2]
-        cveLink = columns[3]
-        results.append({"host": host, "port": port, "cve": cve, "cveLink": cveLink})
-    '''
+   
     # Convert final output into JSON format for frontend formatting
     return jsonify(result)
