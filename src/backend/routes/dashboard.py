ab0894aaa174c49410baa6dbe9862793fd1416c8
briandozie <700042554@student.curtin.edu.my>
2023-09-19 00:20:39 +0800

    implemented history detail page for port scan

--- src/backend/routes/dashboard.py
+++ src/backend/routes/dashboard.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from app import db
+from bson.objectid import ObjectId
 
 dashboard = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 
@@ -20,3 +21,18 @@ def getHistory():
       historyData.append(entry)
 
   return jsonify(historyData)
+
+@dashboard.get("/history/<id>")
+def getHistoryDetails(id):
+  collection = db["activity"]
+  queryResult = collection.find_one({ "_id" : ObjectId(id)},
+    {
+     "activity" : 1,
+     "type": 1,
+     "date": 1,
+     "time": 1,
+     "target": 1,
+     "result": 1,
+     "_id": 0
+    })
+  return queryResult
--- src/frontend/frontend/src/components/HistoryDetail.vue
+++ src/frontend/frontend/src/components/HistoryDetail.vue
@@ -26,24 +26,112 @@
 					<router-link to="/port" class="nav-link">Port Scan</router-link>
 					<router-link to="/dos" class="nav-link">DoS Attack</router-link>
 					<router-link to="/ddos" class="nav-link">DDoS Attack</router-link>
-					<router-link to="/dashboard" class="nav-link">Dashboard</router-link>
+					<router-link to="/dashboard" class="nav-link active"
+						>Dashboard</router-link
+					>
 				</div>
 			</div>
 		</nav>
 
-		<p>This is the History Detail Page</p>
+		<div class="container">
+			<label for="historyLog" class="form-label">History Details</label>
+			<div class="card">
+				<div class="row">
+					<div class="col-4">
+						<h1>Activity</h1>
+						<p>{{ activity.activity }}</p>
+						<h1>Type</h1>
+						<p>{{ activity.type }}</p>
+						<h1>Target Network</h1>
+						<p>{{ activity.target }}</p>
+					</div>
+					<div class="col-8">
+						<h1>Date</h1>
+						<p>{{ activity.date }}</p>
+						<h1>Time</h1>
+						<p>{{ activity.time }}</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="results" class="container">
+			<label for="historyLog" class="form-label">Results</label>
+			<div class="card">
+				<table id="outputTable" class="table table-hover">
+					<thead>
+						<tr>
+							<th scope="col">Port</th>
+							<th scope="col">Status</th>
+							<th scope="col">Protocol</th>
+							<th scope="col">Host</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="result in activity.result" :key="result.port">
+							<td>{{ result.port }}</td>
+							<td>{{ result.status }}</td>
+							<td>{{ result.protocol }}</td>
+							<td>{{ result.host }}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
 	</div>
 </template>
 
-<script></script>
+<script>
+import axios from "axios"
+export default {
+	name: "HistoryDetail",
+	data() {
+		return {
+			activity: {},
+		}
+	},
+	methods: {
+		getHistoryDetails(id) {
+			const path = `http://127.0.0.1:5000/dashboard/history/${id}`
+			axios
+				.get(path)
+				.then((res) => {
+					this.activity = res.data
+				})
+				.catch((err) => {
+					console.log(err)
+				})
+				.finally(() => {})
+		},
+	},
+	mounted() {
+		this.getHistoryDetails(this.$route.params.id)
+	},
+}
+</script>
 
-<style>
+<style scoped>
 .navbar {
 	height: 50px;
 }
 p {
 	display: block;
+	padding-top: 0px;
+	margin-left: 15px;
+}
+.container {
+	padding-top: 50px;
+}
+h1 {
 	padding-top: 10px;
 	margin-left: 15px;
+	font-size: 30px !important;
+}
+table {
+	padding-top: 10px;
+	margin-left: 15px;
+	padding-bottom: 50px;
+}
+#results {
+	margin-bottom: 50px;
 }
 </style>
