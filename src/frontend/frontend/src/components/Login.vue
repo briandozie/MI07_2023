0868a4d5c380b28902ab40a1fc345cbdaffe0e57
Joshua Koh <700042062@student.curtin.edu.my>
2023-09-20 11:31:38 +0800

    Design login page

--- src/frontend/frontend/src/components/Login.vue
+++ src/frontend/frontend/src/components/Login.vue
@@ -1,68 +1,52 @@
 <template>
-	<div>
-		<nav
-			class="navbar bg-dark border-bottom border-bottom-dark"
-			data-bs-theme="dark"
-		>
-			<div class="container-fluid">
-				<router-link class="navbar-brand" to="/home"
-					>SDN Intrusion & Penetration System</router-link
-				>
-				<a class="navbar-brand ms-auto" href="#">
-					<i class="bi bi-gear"></i>
-				</a>
-				<a class="navbar-brand mS-auto" href="#">
-					<i class="bi bi-person"></i>
-				</a>
-			</div>
-		</nav>
+	<div class="login-page">
+		<!-- Blurred background -->
+		<div class="background"></div>
 
-		<nav class="navbar bg-secondary" data-bs-theme="dark">
-			<div class="container-fluid navbar-expand">
-				<div class="nav nav-underline">
-					<router-link to="/cve" class="nav-link">CVE Scan</router-link>
-					<router-link to="/service" class="nav-link">Service Scan</router-link>
-					<router-link to="/ip" class="nav-link">IP Scan</router-link>
-					<router-link to="/port" class="nav-link">Port Scan</router-link>
-					<router-link to="/dos" class="nav-link">DoS Attack</router-link>
-					<router-link to="/ddos" class="nav-link">DDoS Attack</router-link>
-					<!-- <router-link to="/dashboard" class="nav-link">Daskboard</router-link> -->
+		<!-- Pop out container -->
+		<div class="login-container">
+			<h2>Login</h2>
+			<form @submit.prevent="login">
+				<div class="form-group">
+					<label for="username">Username</label>
+					<input type="text" id="username" v-model="username" required />
 				</div>
-			</div>
-		</nav>
-
-		<p>This is the Home Page</p>
-		<!-- <button type="button" class="btn btn-primary">{{ msg }}</button> -->
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" id="password" v-model="password" required />
+				</div>
+			</form>
+		</div>
 	</div>
 </template>
 
 <script>
-// import axios from "axios"
-// export default {
-//   name: "Home",
-//   data() {
-//     return {
-//       msg: "",
-//     }
-//   },
-//   methods: {
-//     getResponse() {
-//       const path = "http://localhost:5000/home"
-//       axios
-//         .get(path)
-//         .then((res) => {
-//           console.log(res.data)
-//           this.msg = res.data
-//         })
-//         .catch((err) => {
-//           console.log(err)
-//         })
-//     },
-//   },
-//   created() {
-//     this.getResponse()
-//   },
-// }
+import axios from "axios"
+export default {
+	name: "Home",
+	data() {
+		return {
+			msg: "",
+		}
+	},
+	methods: {
+		getResponse() {
+			const path = "http://localhost:5000/home"
+			axios
+				.get(path)
+				.then((res) => {
+					console.log(res.data)
+					this.msg = res.data
+				})
+				.catch((err) => {
+					console.log(err)
+				})
+		},
+	},
+	created() {
+		this.getResponse()
+	},
+}
 </script>
 
 <style>
