1840a6868c102162d9f614d24a8f586cdf9cd4ea
briandozie <700042554@student.curtin.edu.my>
2023-10-16 22:55:37 +0800

    removed gear icon

--- src/frontend/frontend/src/components/Home.vue
+++ src/frontend/frontend/src/components/Home.vue
@@ -14,9 +14,6 @@
 					<a class="navbar-brand ms-auto" href="/manual">
 						<i class="bi bi-info-circle"></i>
 					</a>
-					<a class="navbar-brand ms-auto" href="#">
-						<i class="bi bi-gear"></i>
-					</a>
 					<router-link class="navbar-brand ms-auto" to="/login">
 						<i class="bi bi-box-arrow-right"></i> Logout
 					</router-link>
